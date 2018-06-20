deb1ba6de327bc574c54a6d5b6bb41d602b9aef6
ben0110 <a.benarab@hotmail.com>
2018-06-20 17:58:14 +0200

    working on Callcenterorder system

--- ExampleApacheCamel/src/de/tuberlin/cit/vs/orderProcessing/CallCenterOrderSystem.java
+++ ExampleApacheCamel/src/de/tuberlin/cit/vs/orderProcessing/CallCenterOrderSystem.java
@@ -1,5 +1,42 @@
 package de.tuberlin.cit.vs.orderProcessing;
 
-public class CallCenterOrderSystem {
+import java.io.IOException;
+import java.util.Scanner;
 
+import javax.jms.Connection;
+import javax.jms.JMSException;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CallCenterOrderSystem  {
+	
+	
+	public void main(String[] args) {
+	
+		ActiveMQConnectionFactory conFactory = new ActiveMQConnectionFactory();
+        conFactory.setTrustAllPackages(true);
+        try {
+			Connection con = conFactory.createConnection();
+			Scanner in = new Scanner(System.in);
+			con.start();
+			while(!in.nextLine().equals("quit"))
+			{
+			System.out.println("please give your name in this format (firstName LastName nbrofsurfboards nbrOfDivingSuits)");
+			in.nextLine();
+			}
+			con.close();
+			
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+       
+	
+	
+	
+    
+    
+	}
 }
